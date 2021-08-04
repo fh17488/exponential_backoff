efecc38d2b42bb794ee03a3cd5e6f531531e3e23
Faraz Haider <haider.faraz@gmail.com>
2021-08-04 14:06:43 +0100

    first commit

---
+++ exponentialBackoff.js
@@ -0,0 +1,36 @@
+'use strict';
+const AWS = require('aws-sdk');
+const sqs = new AWS.SQS();
+
+module.exports.handler = async (event, context) => {
+  let successful = false;
+
+  for (let record of event.Records) {
+    //successful = Math.random() <= 0.5;
+    try {
+      if (successful) {
+        return {
+          statusCode: 200,
+          body: JSON.stringify(
+            {
+              message: 'successfull!',
+              input: event,
+            },
+            null,
+            2
+          ),
+        };
+      }
+      else
+        throw 'Failed!';
+    } catch (err) {
+      console.log(record);
+      let result = await sqs.getQueueUrl({ QueueName: "InputQueue"}).promise();
+      let visibilityTimeout = Math.pow(2,record.attributes.ApproximateReceiveCount - 1) * 90;
+      let receiptHandle = record.receiptHandle;
+      await sqs.changeMessageVisibility({ QueueUrl: result.QueueUrl, ReceiptHandle: receiptHandle, VisibilityTimeout: visibilityTimeout }).promise();
+      throw 'error';
+    }
+  }
+}
+
